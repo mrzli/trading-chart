09eb391544e67f35f5292c31584bf511f7e75d86
Goran Mržljak <goran.mrzljak@gmail.com>
2024-05-17 17:29:46 +0200

    work on time axis

--- src/chart/axis/time/output-components/year.ts
+++ src/chart/axis/time/output-components/year.ts
@@ -1,3 +1,4 @@
+import { MIN_X_AXIS_TICK_DISTANCE } from '../../../helpers';
 import {
   TimeAxisExtendedDataItem,
   TimeAxisInput,
@@ -8,6 +9,14 @@ export function getTimeAxisOutputYear(
   input: TimeAxisInput,
   items: readonly TimeAxisExtendedDataItem[],
 ): readonly TimeAxisOutputItem[] {
+  const { position, axisLength } = input;
+
+  const { itemSpan } = position;
+
+  const itemsPerPixel = itemSpan / axisLength;
+  const minItemsPerTick = itemsPerPixel * MIN_X_AXIS_TICK_DISTANCE;
+  const finalMinItemsPerTick = Math.max(Math.ceil(minItemsPerTick), 1);
+
   const result = items.map((v, i) => ({
     offset: v.offset,
     value: v.value,
