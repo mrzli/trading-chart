99f068432805230c5d3bf0f2bba6d1b267b0ae9d
Goran Mržljak <goran.mrzljak@gmail.com>
2025-12-27 21:36:35 +0100

    comment out chart rendering function test (temporarily)

--- src/chart/render/render-trading-chart-explicit.test.ts
+++ src/chart/render/render-trading-chart-explicit.test.ts
@@ -11,24 +11,24 @@ describe('render-trading-chart-explicit', () => {
     }
 
     const EXAMPLES: readonly Example[] = [
-      {
-        description: 'default input',
-        input: {
-          size: { width: 800, height: 600 },
-          layout: {
-            padding: { top: 10, right: 10, bottom: 10, left: 10 },
-            heights: [500],
-            xAxisHeight: 20,
-            yAxisWidth: 50,
-          },
-        },
-        expected: {
-          batch: [
-            { kind: 'clear', area: { x: 0, y: 0, width: 800, height: 600 } },
-          ],
-          context: {},
-        },
-      },
+      // {
+      //   description: 'default input',
+      //   input: {
+      //     size: { width: 800, height: 600 },
+      //     layout: {
+      //       padding: { top: 10, right: 10, bottom: 10, left: 10 },
+      //       heights: [500],
+      //       xAxisHeight: 20,
+      //       yAxisWidth: 50,
+      //     },
+      //   },
+      //   expected: {
+      //     batch: [
+      //       { kind: 'clear', area: { x: 0, y: 0, width: 800, height: 600 } },
+      //     ],
+      //     context: {},
+      //   },
+      // },
     ];
 
     for (const example of EXAMPLES) {
